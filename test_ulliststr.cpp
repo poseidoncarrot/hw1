3fe5d82d7de0b658c4bcbd2375859940a5bfb2a3
Matthew Lin <mhlin@usc.edu>
2026-09-11 05:32:37 +0000

    part 2 donegit add .!

--- test_ulliststr.cpp
+++ test_ulliststr.cpp
@@ -9,5 +9,21 @@
 
 int main(int argc, char* argv[])
 {
+  ULListStr list;
 
+  list.push_back("2");
+  list.push_front("1");
+  list.push_back("3");
+
+  std::cout << "Front: " << list.front() << std::endl;
+  std::cout << "Back: " << list.back() << std::endl;
+  std::cout << "Size: " << list.size() << std::endl;
+
+  std::cout << list.get(0) << " " << list.get(1) << " " << list.get(2) << std::endl;
+
+  list.pop_front();
+  list.pop_back();
+
+  std::cout << "Remaining: " << list.front() << std::endl;
+  return 0;
 }
--- ulliststr.cpp
+++ ulliststr.cpp
@@ -24,7 +24,125 @@ size_t ULListStr::size() const
   return size_;
 }
 
-// WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{
+  if (tail_ == nullptr || tail_->last==ARRSIZE) {
+    Item* newItem = new Item;
+
+    newItem->prev = tail_;
+
+    if(tail_ != NULL) {
+      tail_->next = newItem;
+    }
+    else {
+      head_ = newItem;
+    }
+    tail_ = newItem;
+  }
+  tail_->val[tail_->last] = val;
+  tail_->last++;
+  size_++;
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+  if (head_ == nullptr || head_->first == 0) {
+    Item* newItem = new Item;
+
+    newItem->first = ARRSIZE;
+    newItem->last = ARRSIZE;
+    newItem->next = head_;
+
+    if (head_ != nullptr) {
+      head_->prev = newItem;
+    }
+    else {
+      tail_ = newItem;
+    }
+    head_ = newItem;
+  }
+  head_->first--;
+  head_->val[head_->first] = val;
+  size_++;
+}
+
+void ULListStr::pop_back()
+{
+  if (empty()) {
+    return;
+  }
+  tail_->last--;
+  size_--;
+
+  if (tail_->first == tail_->last) {
+    Item* oldTail = tail_;
+    tail_ = tail_->prev;
+
+    if (tail_ != nullptr) {
+      tail_->next = nullptr;
+    }
+    else {
+      head_ = nullptr;
+    }
+    delete oldTail;
+  }
+}
+
+void ULListStr::pop_front()
+{
+  if (empty()) {
+    return;
+  }
+  head_->first++;
+  size_--;
+
+  if (head_->first == head_->last) {
+    Item* oldHead = head_;
+    head_ = head_->next;
+
+    if (head_ != nullptr) {
+      head_->prev = nullptr;
+    }
+    else {
+      tail_ = nullptr;
+    }
+    delete oldHead;
+  }
+}
+
+std::string const& ULListStr::back() const
+{
+  if (empty()) {
+    throw std::invalid_argument("List is empty");
+  }
+  return tail_->val[tail_->last - 1];
+}
+
+std::string const& ULListStr::front() const
+{
+  if (empty()) {
+    throw std::invalid_argument("List is empty");
+  }
+  return head_->val[head_->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  if (loc >= size_) {
+    return nullptr;
+  }
+  Item* current = head_;
+
+  while (current != nullptr) {
+    size_t itemSize = current->last - current->first;
+    if (loc < itemSize) {
+      return &(current->val[current->first + loc]);
+    }
+    loc -= itemSize;
+    current = current->next;
+  }
+  return nullptr;
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
